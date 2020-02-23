9a5e298099c152a953a0e838fff20b5f5e2e57be
Eric Reichert <eric@reichertconsulting.com>
2020-02-23 15:14:03 -0500

    Fixed the max-len problem in eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'max-len': {
-      code: 160
-    }
   },
   overrides: [
     {
