06ebf7e18d25306d054c13f8682e43597737c5ee
Eric Reichert <eric@reichertconsulting.com>
2020-04-08 21:16:11 -0400

    Added tests for handling exceptions when the database is closed and a memory is submitted to the memory service.

--- src/services/MemoryService.ts
+++ src/services/MemoryService.ts
@@ -8,6 +8,13 @@ let dbPromise: Promise<Database>;
 export function initDbWithLocation(location: string): void {
   dbLocation = location;
   dbPromise = sqlite.open(dbLocation);
+  // eslint-disable-next-line no-console
+  console.log(`Opened the database at ${dbLocation}.`);
+}
+
+export async function closeDb(): Promise<void> {
+  const db = await dbPromise;
+  return db.close();
 }
 
 export function initWithConfig(): void {
--- tests/unit/MemoryService.spec.ts
+++ tests/unit/MemoryService.spec.ts
@@ -1,31 +1,37 @@
 import * as MemoryService from '@/services/MemoryService';
-import sqlite from 'sqlite';
 
 describe('When a prompt and memory are submitted to the addMemory function', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     MemoryService.initDbWithLocation('./data/recall_test.db');
   });
 
+  it('should throw an exception if the database is not open.', async () => {
+    await MemoryService.closeDb();
+
+    await expect(MemoryService.addMemory('exception prompt', 'exception memory'))
+      .rejects.toThrow('SQLITE_MISUSE: Database is closed');
+  });
+
   it('the MemoryService should store the memory.', async () => {
-    const returnedId = await MemoryService.addMemory('fifth prompt', 'fifth memory');
+    const returnedId = await MemoryService.addMemory('new prompt', 'new memory');
     expect(returnedId).toBeGreaterThan(0);
   });
 
-  it('sandbox get', async () => {
-    const db = await sqlite.open('./data/recall_test.db');
-    const result = await db.get('SELECT * FROM memory');
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    expect(result).toEqual({ mem_id: 194, prompt: 'fifth prompt', memory: 'fifth memory' });
-    await db.close();
-  });
+  // it('sandbox get', async () => {
+  //   const db = await sqlite.open('./data/recall_test.db');
+  //   const result = await db.get('SELECT * FROM memory');
+  //   // eslint-disable-next-line @typescript-eslint/camelcase
+  //   expect(result).toEqual({ mem_id: 194, prompt: 'fifth prompt', memory: 'fifth memory' });
+  //   await db.close();
+  // });
 
-  it('sandbox on close', async () => {
-    try {
-      const db = await sqlite.open('./data/recall_test.db');
-      await db.get('SELECT * FROM tbl1');
-      await db.close();
-    } catch (err) {
-      console.log(`caught the error ${err}`);
-    }
-  });
+  // it('sandbox on close', async () => {
+  //   try {
+  //     const db = await sqlite.open('./data/recall_test.db');
+  //     await db.get('SELECT * FROM tbl1');
+  //     await db.close();
+  //   } catch (err) {
+  //     console.log(`caught the error ${err}`);
+  //   }
+  // });
 });
