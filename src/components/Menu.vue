99d16933562e52857efc5fa7ce764bc94b652bf2
Eric Reichert <eric@reichertconsulting.com>
2020-04-23 06:54:46 -0400

    Created a Menu component and added it to the App component.

---
+++ src/components/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Menu</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Menu extends Vue {
+}
+</script>
+
+<style scoped>
+</style>
