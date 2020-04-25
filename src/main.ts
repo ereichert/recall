e1f4f79d5b07ea71ae658e45439b8af93fd613a3
Eric Reichert <eric@reichertconsulting.com>
2020-04-25 14:32:14 -0400

    Added vue-router.gs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Vue, { VNode } from 'vue';
 // eslint-disable-next-line import/extensions
 import App from './App.vue';
+import router from './router';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h): VNode => h(App),
 }).$mount('#app');
