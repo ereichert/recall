7a4efb8563655feea204c67a027501f63129c7b5
Eric Reichert <eric@reichertconsulting.com>
2020-04-25 13:44:41 -0400

    Created the menu items.

---
+++ src/components/ActionMenu.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <ul>
+      <li>Add Memory</li>
+      <li>Review Memories</li>
+      <li>Edit Memories</li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ActionMenu extends Vue {
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 9px 0px;
+  }
+  li {
+    padding: 9px 17px;
+    text-align: left;
+    display: inline;
+  }
+</style>
