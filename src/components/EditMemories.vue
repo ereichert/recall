06919b91fc1ba2ec86adc32435fd6a12fe73b98f
Eric Reichert <eric@reichertconsulting.com>
2020-04-26 16:31:08 -0400

    Restructure App to redraw the main content seciton when routed to a new component.

---
+++ src/components/EditMemories.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Edit Memories</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class EditMemories extends Vue {
+}
+</script>
+
+<style>
+</style>
