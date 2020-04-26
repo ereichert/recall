15e017431e3583075774c8d62b83f2cb983ffb1b
Eric Reichert <eric@reichertconsulting.com>
2020-04-26 16:56:55 -0400

    Added tests to ensure the router-links work as expected.

--- tests/unit/ActionMenu.spec.ts
+++ tests/unit/ActionMenu.spec.ts
@@ -1,26 +1,63 @@
-import { render, ComponentHarness } from '@testing-library/vue';
+import { render, ComponentHarness, fireEvent } from '@testing-library/vue';
 import ActionMenu from '@/components/ActionMenu.vue';
 import { routes } from '@/router';
+import App from '@/App.vue';
 
 const renderActionMenu = (): ComponentHarness => render(ActionMenu, { routes });
+const editMemoriesText = 'Edit Memories';
+const reviewMemoriesText = 'Review Memories';
+const addMemoryText = 'Add Memory';
 
 describe('The ActionMenu component', () => {
+  it('should display the Edit Memories page when the Edit Memories action is clicked.', async () => {
+    const { getAllByText } = render(App, { routes });
+    const foundEditMemories = getAllByText(editMemoriesText);
+
+    expect(getAllByText(editMemoriesText).length).toEqual(1);
+
+    await fireEvent.click(foundEditMemories[0]);
+
+    expect(getAllByText(editMemoriesText).length).toEqual(2);
+  });
+
+  it('should display the Review Memories page when the Review Memories action is clicked.', async () => {
+    const { getAllByText } = render(App, { routes });
+    const reviewEditMemories = getAllByText(reviewMemoriesText);
+
+    expect(getAllByText(reviewMemoriesText).length).toEqual(1);
+
+    await fireEvent.click(reviewEditMemories[0]);
+
+    expect(getAllByText(reviewMemoriesText).length).toEqual(2);
+  });
+
+  it('should display the Add Memory page when the Add Memories action is clicked.', async () => {
+    const { getAllByText } = render(App, { routes });
+    const foundAddMemories = getAllByText(addMemoryText);
+
+    expect(getAllByText(addMemoryText).length).toEqual(1);
+
+    await fireEvent.click(foundAddMemories[0]);
+
+    expect(getAllByText(addMemoryText).length).toEqual(2);
+  });
+
   it('should have an Edit Memories action.', () => {
     const { getByText } = renderActionMenu();
 
-    getByText('Edit Memories');
+    getByText(editMemoriesText);
   });
 
   it('should have an Review Memories action.', () => {
     const { getByText } = renderActionMenu();
 
-    getByText('Review Memories');
+    getByText(reviewMemoriesText);
   });
 
   it('should have an AddMemory action.', () => {
     const { getByText } = renderActionMenu();
 
-    getByText('Add Memory');
+    getByText(addMemoryText);
   });
 
   it('The ActionMenu view should render.', () => {
