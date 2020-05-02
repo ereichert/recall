288a5ae37075e004689a6b368cbf33aa348caf6f
Eric Reichert <eric@reichertconsulting.com>
2020-05-02 16:01:07 -0400

    Worked out some of the basics for the memory review screen.

--- src/components/MemoryReview.vue
+++ src/components/MemoryReview.vue
@@ -2,17 +2,35 @@
   <div>
     <h1>Memory Review</h1>
     <label>Prompt</label>
-    <p>Prompt Placeholder</p>
-    <label>Memory Details</label>
-    <p>Memory Placeholder</p>
+    <p>{{ currentMemoryRecordPrompt() }}</p>
+    <label v-show="showMemoryDetails">Memory Details</label>
+    <p v-show="showMemoryDetails">Memory Placeholder</p>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import MemoryRecordReview from '@/models/MemoryRecordReview';
+import MemoryRecord from '../models/MemoryRecord';
 
 @Component
 export default class MemoryReview extends Vue {
+  currentMemoryRecord: MemoryRecordReview | null = new MemoryRecordReview(
+    new MemoryRecord('this is the prompt', 'these are the details'),
+  );
+
+  showMemoryDetails = true;
+
+  memoriesToBeReviewed: Array<MemoryRecordReview> = [];
+
+  currentMemoryRecordPrompt(): string {
+    let prompt = '';
+    if (this.currentMemoryRecord && this.currentMemoryRecord.memoryRecord) {
+      prompt = this.currentMemoryRecord.memoryRecord.prompt;
+    }
+
+    return prompt;
+  }
 }
 </script>
 
