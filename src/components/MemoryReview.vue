848650ee249917ba4864e58606e572edae28cf50
Eric Reichert <eric@reichertconsulting.com>
2020-05-09 15:04:37 -0400

    Added styling to the controls area.

--- src/components/MemoryReview.vue
+++ src/components/MemoryReview.vue
@@ -7,26 +7,31 @@ Test me
       <label for='prompt-text'>Prompt</label>
       <p id='prompt-text'>{{ currentMemoryRecordPrompt() }}</p>
     </div>
-    <div v-if="shouldShowMemoryDetails">
-      <label for='details-text'>Memory Details</label>
-      <p id='details-text'>memory details placeholder</p>
+    <div id="details-area">
+      <div v-if="shouldShowMemoryDetails">
+        <label for='details-text'>Memory Details</label>
+        <p id='details-text'>memory details placeholder</p>
+      </div>
     </div>
-    <div>
-      <div v-if="shouldShowMemoryDetailsButton">
+    <div id="control-area">
+      <div class="control-buttons" v-if="shouldShowMemoryDetailsButton">
         <button
           type="submit"
           @click="showMemoryDetails"
         >Show Memory Details</button>
       </div>
-      <div v-if="shouldShowMemoryResolutionButtons">
+      <div class="control-buttons" v-if="shouldShowMemoryResolutionButtons">
         <button
           type="submit"
+          class="resolution-button"
         >1</button>
         <button
           type="submit"
+          class="resolution-button"
         >2</button>
         <button
           type="submit"
+          class="resolution-button"
         >4</button>
       </div>
     </div>
@@ -82,7 +87,21 @@ export default class MemoryReview extends Vue {
 
 <style scoped>
   #prompt-area {
-    border-bottom: solid gray;
+    border-bottom: thin solid gray;
     margin-bottom: 10px;
+  }
+  #details-area {
+    height: 200px;
+  }
+  #control-area {
+    height: 50px;
+    background: lightgray;
+    display: table;
+    width: 100%
+  }
+  .control-buttons {
+    height: 100%;
+    display: table-cell;
+    vertical-align: middle;
   }
 </style>
