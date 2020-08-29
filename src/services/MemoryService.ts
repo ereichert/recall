f8f4dcae1731928ee3ca04f658b66b5f903f8f77
Eric Reichert <eric@reichertconsulting.com>
2020-08-29 16:36:30 -0400

    Got rid of electron and moved back to a pure web based model.

--- src/services/MemoryService.ts
+++ src/services/MemoryService.ts
@@ -1,22 +1,23 @@
-import sqlite, { Database } from 'sqlite';
+// import sqlite, { Database } from 'sqlite';
 import MemoryRecord from '@/models/MemoryRecord';
 import MemoryRecordReview from '@/models/MemoryRecordReview';
 import * as ConfigService from './ConfigService';
 import { ConfigKeys } from './ConfigService';
 
 let dbLocation: string;
-let dbPromise: Promise<Database>;
+// let dbPromise: Promise<Database>;
 
 export function initDbWithLocation(location: string): void {
   dbLocation = location;
-  dbPromise = sqlite.open(dbLocation);
+  // dbPromise = sqlite.open(dbLocation);
   // eslint-disable-next-line no-console
   console.log(`Opened the database at ${dbLocation}.`);
 }
 
 export async function closeDb(): Promise<void> {
-  const db = await dbPromise;
-  return db.close();
+  // const db = await dbPromise;
+  // return db.close();
+  return Promise.resolve();
 }
 
 export function initWithConfig(): void {
@@ -27,13 +28,15 @@ export function initWithConfig(): void {
 }
 
 export async function addMemory(memoryRecord: MemoryRecord): Promise<number> {
-  const db = await dbPromise;
-  return db.run('INSERT INTO memories(prompt, details) values(?, ?);', memoryRecord.prompt, memoryRecord.details)
-    .then((statement) => statement.lastID);
+  // const db = await dbPromise;
+  // return db.run('INSERT INTO memories(prompt, details) values(?, ?);', memoryRecord.prompt, memoryRecord.details)
+  // .then((statement) => statement.lastID);
+  return Promise.resolve(0);
 }
 
 export async function getAllMemoryRecordReviews(): Promise<MemoryRecordReview[]> {
-  const db = await dbPromise;
-  const memories = await db.all('SELECT * FROM memories;');
-  return memories.map((memoryRow) => new MemoryRecordReview(memoryRow));
+  // const db = await dbPromise;
+  // const memories = await db.all('SELECT * FROM memories;');
+  // return memories.map((memoryRow) => new MemoryRecordReview(memoryRow));
+  return Promise.resolve(new Array(new MemoryRecordReview(new MemoryRecord())));
 }
